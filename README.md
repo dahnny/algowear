86abe8fb712fd941206de9388258ce682f4c642d
dahnny <danielogbuti@gmail.com>
2023-02-07 08:30:48 +0100

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+Algo wear is an algorand dapp that enables people purchase clothes on the algorand blockchain. 
+1. The owner of the dapp can apply discounts to his/her item when the discount is 0 it means no discount is applied. 
+2. Also owners of items can update the stock incase it runs out. 
+3. Owners of items can also change the discount on items as they see fit
+4. Once an item runs out, it cant be bought until the owner updates the stock
+
+Demo: https://shimmering-caramel-7a67ca.netlify.app/
+
---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+import algosdk from "algosdk";
+import MyAlgoConnect from "@randlabs/myalgo-connect";
+
+const config = {
+    algodToken: "",
+    algodServer: "https://testnet-api.algonode.network",
+    algodPort: "",
+    indexerToken: "",
+    indexerServer: "https://testnet-idx.algonode.network",
+    indexerPort: "",
+}
+
+export const algodClient = new algosdk.Algodv2(config.algodToken, config.algodServer, config.algodPort)
+
+export const indexerClient = new algosdk.Indexer(config.indexerToken, config.indexerServer, config.indexerPort);
+
+export const myAlgoConnect = new MyAlgoConnect();
+
+export const minRound = 25556983;
+
+// https://github.com/algorandfoundation/ARCs/blob/main/ARCs/arc-0002.md
+export const marketplaceNote = "wear:uv3"
+
+// Maximum local storage allocation, immutable
+export const numLocalInts = 0;
+export const numLocalBytes = 0;
+// Maximum global storage allocation, immutable
+export const numGlobalInts = 3; // Global variables stored as Int: count, sold
+export const numGlobalBytes = 4; // Global variables stored as Bytes: name, description, image
+
+export const ALGORAND_DECIMALS = 6;
